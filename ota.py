58b75a8dad201a2ec4fcf5d1b01ac676c01ba400
gitpsylab <dustusonaboumoni@gmail.com>
2026-02-04 03:12:57 +0530

    Micropython OTA - Probe and Update

--- ota.py
+++ ota.py
@@ -1,1 +1,75 @@
+import urequests
+import uhashlib
+
+
+class ota:
+    raw = "https://raw.githubusercontent.com"
+    github = "https://github.com"
+
+    def __init__(self, user, repo, url=None, branch="main", working_dir="zen", files=["boot.py", "main.py"], headers={'User-Agent': 'zen-gitpsylab'}):
+        self.base_url = "{}/{}/{}".format(self.raw, user, repo) if user else url.replace(self.github, self.raw)
+        self.url = url if url is not None else "{}/{}/{}".format(self.base_url, branch, working_dir)
+        self.headers = headers
+        self.files = files
+
+    def _check_hash(self, x, y):
+        x_hash = uhashlib.sha1(x.encode())
+        y_hash = uhashlib.sha1(y.encode())
+
+        x = x_hash.digest()
+        y = y_hash.digest()
+
+        if str(x) == str(y):
+            return True
+        else:
+            return False
+
+    def _get_file(self, url):
+        payload = urequests.get(url, headers=self.headers)
+        code = payload.status_code
+
+        if code == 200:
+            return payload.text
+        else:
+            return None
+
+    def _check_all(self):
+        changes = []
+
+        for file in self.files:
+            latest_version = self._get_file(self.url + "/" + file)
+            if latest_version is None:
+                continue
+
+            try:
+                with open(file, "r") as local_file:
+                    local_version = local_file.read()
+            except:
+                local_version = ""
+
+            if not self._check_hash(latest_version, local_version):
+                changes.append(file)
+
+        return changes
+
+    def probe(self):
+        # Returns true if at least one file update available
+        if not self._check_all():
+            return False
+        else:
+            return True
+
+    def update(self):
+        # Updates and returns true if at least one file is updated
+        changes = self._check_all()
+
+        for file in changes:
+            with open(file, "w") as local_file:
+                local_file.write(self._get_file(self.url + "/" + file))
+
+        if changes:
+            return True
+        else:
+            return False
+
 
