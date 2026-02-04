01529afdd0f7aa9c7221e5e4f79b885a1165c974
gitpsylab <dustusonaboumoni@gmail.com>
2026-02-04 19:00:23 +0530

    update boot.py

--- zen/boot.py
+++ zen/boot.py
@@ -1,1 +1,17 @@
+import ota
+import machine
 
+
+ota = ota.ota(
+  user="gitpsylab",
+  repo="zen",
+  branch="main",
+  working_dir="zen",
+  files = ["boot.py", "main.py"]
+)
+
+ota.wificonnect()
+
+if ota.update():
+    print("update complete.. rebooting...")
+    machine.reset()
